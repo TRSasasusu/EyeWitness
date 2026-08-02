51d51a7343710f1581c00a005978e534f3df2839
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2026-08-02 14:12:40 +0900

    initial commit

---
+++ EyeWitness/EyeWitness.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace EyeWitness {
+    public class EyeWitness : ModBehaviour {
+        public static EyeWitness Instance;
+        public INewHorizons NewHorizons;
+
+        public static void Log(string text, MessageType messageType = MessageType.Message) {
+            Instance.ModHelper.Console.WriteLine(text, messageType);
+        }
+
+        public void Awake() {
+            Instance = this;
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        public void Start() {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(EyeWitness)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizons.LoadConfigs(this);
+
+            new Harmony("orclecle.EyeWitness").PatchAll(Assembly.GetExecutingAssembly());
+
+            // Example of accessing game code.
+            OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
+            LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+        }
+
+        public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene) {
+            if (newScene != OWScene.SolarSystem) return;
+            ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+        }
+    }
+
+}
