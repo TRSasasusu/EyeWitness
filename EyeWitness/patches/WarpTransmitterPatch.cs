58a8d035662bc16c6012a04d614b1dadf4dc844f
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2026-08-09 23:08:52 +0900

    make warp transmitter in DB with patches for warping issue

---
+++ EyeWitness/patches/WarpTransmitterPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniRx;
+
+namespace EyeWitness.patches {
+    [HarmonyPatch]
+    public static class WarpTransmitterPatch {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(NomaiWarpTransmitter), nameof(NomaiWarpTransmitter.ReceiveWarpedBody))]
+        public static void NomaiWarpTransmitter_ReceiveWarpedBody_Postfix(NomaiWarpTransmitter __instance) {
+            if(__instance.name == "warpTransmitter_db_datura") {
+                EyeWitness.Log("Patch ReceiveWarpedBody");
+                __instance._objectsOnPlatform.Clear(); // avoid continuous warp. specifically, in DB, continuous warp causes DB entrance warp in some reason.
+                Observable.NextFrame().Subscribe(_ => {
+                    Observable.NextFrame().Subscribe(_ => {
+                        __instance._objectsOnPlatform.Clear();
+                    }).AddTo(__instance);
+                    Observable.TimerFrame(2, FrameCountType.FixedUpdate).Subscribe(_ => {
+                        __instance._objectsOnPlatform.Clear();
+                    }).AddTo(__instance);
+                }).AddTo(__instance);
+            }
+        }
+    }
+}
